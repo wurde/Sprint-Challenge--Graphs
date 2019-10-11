f5cd986e7e5d2567505bba4f0897164176d4262b
Andy Bettisworth <andy@accreu.com>
2019-10-10 20:47:30 -0500

    Abstract graphs into their own file

--- graph_adventure/world.py
+++ graph_adventure/world.py
@@ -21,15 +21,19 @@ class World:
         numRooms = len(roomGraph)
         rooms = [None] * numRooms
         gridSize = 1
+
         for i in range(0, numRooms):
             x = roomGraph[i][0][0]
             gridSize = max(gridSize, roomGraph[i][0][0], roomGraph[i][0][1])
             self.rooms[i] = Room(f"Room {i}", f"({roomGraph[i][0][0]},{roomGraph[i][0][1]})",i, roomGraph[i][0][0], roomGraph[i][0][1])
+
         self.roomGrid = []
         gridSize += 1
         self.gridSize = gridSize
+
         for i in range(0, gridSize):
             self.roomGrid.append([None] * gridSize)
+
         for roomID in roomGraph:
             room = self.rooms[roomID]
             self.roomGrid[room.x][room.y] = room
@@ -41,6 +45,7 @@ class World:
                 self.rooms[roomID].connectRooms('e', self.rooms[roomGraph[roomID][1]['e']])
             if 'w' in roomGraph[roomID][1]:
                 self.rooms[roomID].connectRooms('w', self.rooms[roomGraph[roomID][1]['w']])
+
         self.startingRoom = self.rooms[0]
 
     def printRooms(self):
